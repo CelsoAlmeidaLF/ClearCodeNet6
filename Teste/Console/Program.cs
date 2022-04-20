9eeebb9f1857f2fe1721623acee62fd94d18c02f
Laughing Man <celso.almeida.leite@hotmail.com>
2022-04-19 22:02:13 -0300

    BusinessTest: Insert, Update, Delete.

--- Teste/Console/Program.cs
+++ Teste/Console/Program.cs
@@ -5,25 +5,15 @@ using System.Diagnostics;
 
 Console.Title = "Console Test CRUD";
 
-// Entidades do negócio.
-Entity entity = new Entity();
-
 // Chama regra de negócio.
 IBusiness<Entity> _business = new BusinessOffice();
 IlogEvent log = new Log();
 
-#region "Methods basic CRUD"
+Console.WriteLine("Name: " + _business.GetType().Name);
+Console.WriteLine("Assembly: " + _business.GetType().Assembly);
+Console.ReadKey();
 
-//entity = new Entity() { DataCriação = DateTime.Now, ID = 402 };
-entity.Valid = _business.InsertRepository(entity);
-Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} cadastrado em: {entity.DataCriação}." : "não cadastrado ..."));
+Debug.WriteLine("--> INICIO ");
 
-//entity = new Entity() { DataAlteração = DateTime.Now, ID = 404 };
-entity.Valid = _business.UpdateRepository(entity);
-Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} alterado em: {entity.DataAlteração}." : "não alterado ..."));
 
-//entity = new Entity() { DataExclusão = DateTime.Now, ID = 405 };
-entity.Valid = _business.DeleteRepository(entity);
-Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} removido em: {entity.DataExclusão}." : "não removido ..."));
-
-#endregion
+Debug.WriteLine("--> FIM ");
--- Teste/NUT_Application/Business/TestBusiness.cs
+++ Teste/NUT_Application/Business/TestBusiness.cs
@@ -1,27 +1,53 @@
 using NUnit.Framework;
-using Systekna;
-
+using Systekna.Application;
 using Systekna.Core.Entity;
 using System;
-using System.Data;
+using System.Diagnostics;
 
 namespace NUT.Application
 {
     public class TestsBusiness
     {
+        // Chama regra de negócio.
+        private IBusiness<Entity> _business;
+
         [SetUp]
         public void Setup()
         {
+            _business = new BusinessOffice();
         }
 
         [Test]
-        public void Test1()
+        public void TestInsert()
         {
+            Entity entity = new Entity();
+            entity = new Entity() { DataCriação = DateTime.Now, ID = 402 };
+            entity.Valid = _business.InsertRepository(entity);
+
+            Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} cadastrado em: {entity.DataCriação}." : "não cadastrado ..."));
+            Assert.IsTrue(entity.GetValid);
         }
 
         [Test]
-        public void Test2()
+        public void TestUpdate()
         {
+            Entity entity = new Entity();
+            entity = new Entity() { DataAlteração = DateTime.Now, ID = 404 };
+            entity.Valid = _business.UpdateRepository(entity);
+
+            Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} alterado em: {entity.DataAlteração}." : "não alterado ..."));
+            Assert.IsTrue(entity.GetValid);
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            Entity entity = new Entity();
+            entity = new Entity() { DataExclusão = DateTime.Now, ID = 405 };
+            entity.Valid = _business.DeleteRepository(entity);
+
+            Debug.WriteLine((entity.GetValid ? $"ID: {entity.ID} removido em: {entity.DataExclusão}." : "não removido ..."));
+            Assert.IsTrue(entity.GetValid);
         }
     }
 }
